5a6c49b3c91243dff5f718a16d687f7b3cf3952a
HaroruDa3 <harol.kock34@gmail.com>
2025-08-14 21:49:29 -0600

    actualizacion singleton firebase admin

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,3 +1,4 @@
+import "server-only"; 
 import { initializeApp, getApps, cert } from "firebase-admin/app"
 import { getAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
